448bdc4524ca079b47997fd52f76f9c721b02689
Abbas Angouti <g.a.angouti@gmail.com>
2018-11-23 14:49:28 -0800

    Initial Commit

---
+++ LyftChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LyftChallenge
+//
+//  Created by Abbas Angouti on 11/23/18.
+//  Copyright © 2018 Abbas Angouti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
